e15e4a366c4d454a439b06eef75007d3468b4db6
sam <morvazhu@gmail.com>
2017-04-18 15:28:06 +0800

    create action and index view

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -9,6 +9,11 @@ class PlaysController < ApplicationController
 
   def create
     @play = Play.new(play_params)
+    if @play.save
+      redirect_to root_path
+    else
+      render 'new'
+    end    
   end
 
   private
